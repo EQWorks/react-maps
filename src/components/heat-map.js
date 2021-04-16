23988d16d388145a8028e662c1009445290ee928
DoParkEQ <do.park@eqworks.com>
2021-04-16 14:47:52 -0400

    heatmap - component replacement with generic map

--- src/components/heat-map.js
+++ src/components/heat-map.js
@@ -1,18 +1,8 @@
 import PropTypes from 'prop-types'
-import React from 'react'
-import DeckGL from '@deck.gl/react'
+import React, { useMemo } from 'react'
 import { HeatmapLayer } from '@deck.gl/aggregation-layers'
-import { StaticMap } from 'react-map-gl'
-// import { FlyToInterpolator } from "deck.gl";
+import Map from './generic-map'
 import * as d3 from 'd3-scale'
-
-const INITIAL_VIEW_STATE = {
-  longitude: -112.8452,
-  latitude: 49.6956,
-  zoom: 13,
-  pitch: 0,
-  bearing: 0,
-}
 
 const integratedScale = (domain, range, value) => {
   const scale = d3.scaleLinear()
@@ -23,33 +13,28 @@ const integratedScale = (domain, range, value) => {
 
 const HeatMap = ({ reportData, mapboxApiAccessToken }) => {
 
-  const layers = new HeatmapLayer({
-    id: 'heatmap',
-    data: reportData,
-    radiusPixels: 100,
-    opacity: 0.4,
-    threshold: 0.2,
-    pickable: false,
-    visible: true,
-    getPosition: (d) => [d.lon, d.lat],
-    getWeight: (d) => {
-      return integratedScale([0, 550], [1, 100], d.converted_visits)
-    },
-  })
-
+  const layers = useMemo(() => ([
+    new HeatmapLayer({
+      id: 'heatmap',
+      data: reportData,
+      radiusPixels: 100,
+      opacity: 0.4,
+      threshold: 0.2,
+      pickable: false,
+      visible: true,
+      getPosition: (d) => [d.lon, d.lat],
+      getWeight: (d) => {
+        return integratedScale([0, 550], [1, 100], d.converted_visits)
+      },
+    }),
+  ]))
 
   return (
     <div>
-      <DeckGL
-        initialViewState={INITIAL_VIEW_STATE}
-        controller={true}
+      <Map
         layers={layers}
-      >
-        <StaticMap
-          preventStyleDiffing={true}
-          mapboxApiAccessToken={mapboxApiAccessToken}
-        />
-      </DeckGL>
+        mapboxApiAccessToken={mapboxApiAccessToken}
+      />
     </div>
   )
 }
