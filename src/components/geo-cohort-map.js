61ad1f98862294d7e096c01e86ab05d29ee2b9c3
Erika Szabo <erika.szabo.geo@gmail.com>
2021-04-26 19:51:55 -0400

    GeoCohortMap - add metricAliases prop

--- src/components/geo-cohort-map.js
+++ src/components/geo-cohort-map.js
@@ -267,10 +267,19 @@ const GeoCohortMap = ({
 
   const legends = useLegends({ elevationBasedOn, fillBasedOn, fillColors, objColor, metrics })
 
-  const legend = useMemo(() => (
-    showLegend &&
-    (legendNode || <Legend legends={legends} position={legendPosition} typograpy={typography}/>)
-  ), [showLegend, legends, legendPosition, typography, legendNode])
+  const legend = useMemo(() => {
+    const { metricAliases } = tooltipKeys
+    return (
+      showLegend &&
+      (legendNode ||
+        <Legend
+          legends={legends}
+          metricAliases={metricAliases}
+          position={legendPosition}
+          typograpy={typography}
+        />
+      )
+    )}, [showLegend, legends, tooltipKeys,legendPosition, typography, legendNode])
 
   return (
     <Map
